3e5dc8be323c692cbddca1e809357877c38ba97e
Anton <130680186+Toxcorp@users.noreply.github.com>
2025-08-07 14:48:03 +0300

    Update 1task.py

--- 1task.py
+++ 1task.py
@@ -5,13 +5,14 @@ while True:
     n4 = int(input("Enter fourth nunber:"))
     try:
         max_price = n1
-        if n2 > n1:
+        if n2 > max_price:
             max_price = n2
-        if n3 > n2:
+        if n3 > max_price:
             max_price = n3
-        if n4 > n3:
+        if n4 > max_price:
             max_price = n4
         print("Max price is", max_price)
     finally:
         print("Another example?")
+
     print()
