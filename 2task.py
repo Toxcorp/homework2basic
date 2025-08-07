4a0c63c3a0369a83ddafce06b1ff772057ed06b8
Anton <130680186+Toxcorp@users.noreply.github.com>
2025-08-07 14:55:27 +0300

    Update 2task.py

--- 2task.py
+++ 2task.py
@@ -3,26 +3,28 @@ while True:
     floor_total = 9
     flatpfloor = 4
     try:
-        x = (flat_number/flatpfloor)
-        if (x > floor_total):
-            y = (x / 2)
-            if (y > 9):
-                z = x / 3
-                if (z > 9):
-                    j = floor_total - ((144 - flat_number) // 4)
-                    print("Part 4, floor:", int(j))
+        if flat_number > 0 and flat_number <= (floor_total * flatpfloor * house_parts):    
+            x = (flat_number/flatpfloor)
+            if (x > floor_total):
+                y = (x / 2)
+                if (y > 9):
+                    z = x / 3
+                    if (z > 9):
+                        j = floor_total - ((144 - flat_number) // 4)
+                        print("Part 4, floor:", int(j))
+                    else:
+                        h = floor_total - ((108 - flat_number) //4)
+                        print("Part 3, floor:", int(h))
                 else:
-                    h = floor_total - ((108 - flat_number) //4)
-                    print("Part 3, floor:", int(h))
+                    f = floor_total - ((72 - flat_number) // 4)
+                    print("Part 2, floor:", int(f))
             else:
-                f = floor_total - ((72 - flat_number) // 4)
-                print("Part 2, floor:", int(f))
+                if flat_number % 4 != 0:
+                    t = ((x+1) // 1)
+                    print("Part 1, floor:", int(t))
+                else:
+                    print("Part 1, floor:",int(x))
         else:
-            if flat_number % 4 != 0:
-                t = ((x+1) // 1)
-                print("Part 1, floor:", int(t))
-            else:
-                print("Part 1, floor:",int(x))
-    
+            print("No such flat in that house")
     finally:
         print()
